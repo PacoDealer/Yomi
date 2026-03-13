e355cab5556ba83172abdb16c43839390b40f056
PacoDealer <tintin@gamberg.com.ar>
2026-03-13 20:33:42 -0300

    init: proyecto Yomi + documentación inicial

---
+++ Yomi/YomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomiApp.swift
+//  Yomi
+//
+//  Created by Martin Gamberg on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct YomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
